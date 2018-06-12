c1dcbc7ed1cdc2b3d302bb5043db1cc08b098dd4
yaoqifa <yaoqifa@bilibili.com>
2018-06-12 15:35:17 +0800

    +doubanApi, using request and request-promise-native

---
+++ server/tasks/api.js
@@ -0,0 +1,28 @@
+const rp = require('request-promise-native')
+
+async function fetchMovie (item) {
+  const url = `http://api.douban.com//v2/movie/subject/${item.doubanId}`
+  const res = await rp(url)
+
+  return res
+}
+
+;(async () => {
+  let movies = [
+    { "doubanId": 26955451,
+    "title": "詹姆斯·卡梅隆的科幻故事",
+    "rate": 9.4,
+    "poster": "https://img3.doubanio.com/view/photo/l_ratio_poster/public/p2523480904.jpg"
+    },
+    { "doubanId": 27043263,
+      "title": "福尔摩斯：基本演绎法 第六季",
+      "rate": 9.1,
+      "poster": "https://img3.doubanio.com/view/photo/l_ratio_poster/public/p2382097744.jpg" 
+    }
+  ]
+
+  movies.forEach(async m => {
+    let movieData = await fetchMovie(m)
+    console.log(movieData)
+  })
+})()
